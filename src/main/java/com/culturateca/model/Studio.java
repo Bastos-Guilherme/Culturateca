df9af50551ce54c668ca1302798ea6b8cf69fdbd
Guilherme Bastos <98905074+Bastos-Guilherme@users.noreply.github.com>
2022-06-17 17:30:30 -0300

    Studio now consumes Lombok

--- src/main/java/com/culturateca/model/Studio.java
+++ src/main/java/com/culturateca/model/Studio.java
@@ -3,7 +3,15 @@ package com.culturateca.model;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Studio {
 
@@ -15,39 +23,4 @@ public class Studio {
     private MasterPiece masterPiece;
     @OnetoOne
     private Adress hqAdress;
-
-    public Studio() {
-    }
-
-    public String getCompanyName() {
-        return companyName;
-    }
-
-    public void setCompanyName(String companyName) {
-        this.companyName = companyName;
-    }
-
-    public LocalDateTime getFoundationDate() {
-        return foundationDate;
-    }
-
-    public void setFoundationDate(LocalDateTime foundationDate) {
-        this.foundationDate = foundationDate;
-    }
-
-    public MasterPiece getMasterPiece() {
-        return masterPiece;
-    }
-
-    public void setMasterPiece(MasterPiece masterPiece) {
-        this.masterPiece = masterPiece;
-    }
-
-    public Adress getHqAdress() {
-        return hqAdress;
-    }
-
-    public void setHqAdress(Adress hqAdress) {
-        this.hqAdress = hqAdress;
-    }
 }
