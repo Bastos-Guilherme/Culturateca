80cb549bd3c4fac9d27131af802d2cb2a2567e46
Guilherme Bastos <98905074+Bastos-Guilherme@users.noreply.github.com>
2022-06-17 17:25:22 -0300

    Update Category.java

--- src/main/java/com/culturateca/model/Category.java
+++ src/main/java/com/culturateca/model/Category.java
@@ -3,6 +3,10 @@ package com.culturateca.model;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
