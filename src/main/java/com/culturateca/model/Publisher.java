837dcaa8d55355a2f687a8a4285a6a59a761dc44
Guilherme Bastos <98905074+Bastos-Guilherme@users.noreply.github.com>
2022-06-17 17:29:53 -0300

    Publisher now consumes Lombok

--- src/main/java/com/culturateca/model/Publisher.java
+++ src/main/java/com/culturateca/model/Publisher.java
@@ -4,7 +4,15 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Publisher {
 
@@ -16,15 +24,4 @@ public class Publisher {
     private Adress adress;
     @ManytoOne
     private MasterPiece masterPiece;
-
-    public Publisher() {
-    }
-
-    public String getPublisherName() {
-        return publisherName;
-    }
-
-    public void setPublisherName(String publisherName) {
-        this.publisherName = publisherName;
-    }
 }
