158118b81c923e6dbe54c96da3dfb6a6fcd92470
Guilherme Bastos <98905074+Bastos-Guilherme@users.noreply.github.com>
2022-06-17 17:18:32 -0300

    Author now consumes Lombok

--- src/main/java/com/culturateca/model/Author.java
+++ src/main/java/com/culturateca/model/Author.java
@@ -7,6 +7,10 @@ import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Entity
+@Data
+@NoArgsContructor
+@AllArgsContructor
+@Builder
 public class Author {
 
     @Id
@@ -20,59 +24,4 @@ public class Author {
     private String nationality;
     @ManytoMany
     private MasterPiece masterPiece;
-
-    public Author() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public LocalDateTime getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDateTime birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public LocalDateTime getDeathDate() {
-        return deathDate;
-    }
-
-    public void setDeathDate(LocalDateTime deathDate) {
-        this.deathDate = deathDate;
-    }
-
-    public String getNationality() {
-        return nationality;
-    }
-
-    public void setNationality(String nationality) {
-        this.nationality = nationality;
-    }
-
-    public MasterPiece getMasterPiece() {
-        return masterPiece;
-    }
-
-    public void setMasterPiece(MasterPiece masterPiece) {
-        this.masterPiece = masterPiece;
-    }
 }
