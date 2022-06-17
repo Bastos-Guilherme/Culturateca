c278ca974360aa1ec00bc3bd5f4d4731353f0ff6
Guilherme Bastos <98905074+Bastos-Guilherme@users.noreply.github.com>
2022-06-17 17:25:51 -0300

    Update Author.java

--- src/main/java/com/culturateca/model/Author.java
+++ src/main/java/com/culturateca/model/Author.java
@@ -5,6 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
