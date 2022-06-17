15a25765c6de217992ce92477baae3557fe284b1
Guilherme Bastos <98905074+Bastos-Guilherme@users.noreply.github.com>
2022-06-17 16:52:31 -0300

    Update SwaggerConfig.java

--- src/main/java/com/culturateca/config/swagger/SwaggerConfig.java
+++ src/main/java/com/culturateca/config/swagger/SwaggerConfig.java
@@ -9,6 +9,8 @@ import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
+    
+    public SwaggerConfig() {}
 
     @Bean
     public Docket culturatecaApi() {
