a39998ceadc35b84006d9318465b508700aa4526
Guilherme Bastos <gui_427@hotmail.com>
2022-08-04 01:36:04 -0300

    added Type to model

--- src/main/java/com/culturateca/model/Type.java
+++ src/main/java/com/culturateca/model/Type.java
@@ -1,4 +1,21 @@
 package com.culturateca.model;
 
 public enum Type {
+
+    BOOK("book"),
+    HANDOUT("handout"),
+    BLUE_RAY("blue-ray"),
+    CD("cd"),
+    DVD("dvd"),
+    K7("k7"),
+    MAP("map"),
+    MAGAZINE("magazine"),
+    MANGA("manga"),
+    LP("lp");
+
+    public final String value;
+
+    Type(String value) {
+        this.value = value;
+    }
 }
